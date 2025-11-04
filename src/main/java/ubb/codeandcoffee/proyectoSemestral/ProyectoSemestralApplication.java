4cd2b2c1852e759419d54ecbe0066af552c2f34b
FValladares2 <116167241+FValladares2@users.noreply.github.com>
2025-11-03 21:04:27 -0300

    archivos springboot base

---
+++ src/main/java/ubb/codeandcoffee/proyectoSemestral/ProyectoSemestralApplication.java
@@ -0,0 +1,13 @@
+package ubb.codeandcoffee.proyectoSemestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemestralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ubb/codeandcoffee/proyectoSemestral/ProyectoSemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package ubb.codeandcoffee.proyectoSemestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
